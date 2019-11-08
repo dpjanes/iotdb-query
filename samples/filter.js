e6e39e0b86a0a0932eebf7dcf8428468c932b615
David Janes <david@consensas.com>
2019-11-08 11:45:49 -0500

    add filtering functions

---
+++ samples/filter.js
@@ -0,0 +1,21 @@
+const _ = require("iotdb-helpers")
+const query = require("..")
+const movies = require("./movies.json")
+
+let qd = {
+    // "info/directors": "Ron Howard",
+    "info/actors": "Morgan Freeman",
+    "year": [ "<=", 2000 ],
+}
+
+_.promise({
+    jsons: movies,
+    query: qd,
+})
+    .then(query.filter)
+    .make(sd => {
+        console.log(JSON.stringify(sd.jsons, null, 2))
+    })
+    .catch(error => {
+        console.log("#", _.error.messsage(error))
+    })
