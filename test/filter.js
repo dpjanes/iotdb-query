9fe53e39a0079c7c7f2458966deef581c0d1441e
David Janes <david@consensas.com>
2019-11-19 05:00:14 -0500

    test filter function

---
+++ test/filter.js
@@ -0,0 +1,59 @@
+/**
+ *  test/filter.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2018-11-19
+ *
+ *  Copyright (2013-2020) David P. Janes
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+const _ = require("iotdb-helpers")
+const query = require("..")
+
+const assert = require("assert")
+
+const movies = require("./data/movies.json")
+
+/*
+let qd = {
+    // "info/directors": "Ron Howard",
+    // "info/actors": "Morgan Freeman",
+    // "year": [ "<=", 2000 ],
+    "year": [ "not-in", 2000, 2013 ],
+}
+*/
+
+
+describe("test", function() {
+    it("all", function(done) {
+        _.promise({
+            jsons: movies,
+            query: {
+            },
+        })
+            .then(query.filter)
+            .make(sd => {
+                const got = sd.jsons.length
+                const expected = 88
+
+                assert.strictEqual(got, expected)
+                assert.ok(_.is.Array.of.Dictionary(sd.jsons))
+            })
+            .end(done)
+    })
+})
